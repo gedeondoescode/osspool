cacd229bb6295f94533c26ed5181531f02cc4c97
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-07-13 14:47:21 -0400

    Setup Prisma and Supabase

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<h1 className="font-bold text-5xl text-red-500">Hell</h1>
+		</div>
+	);
+}
