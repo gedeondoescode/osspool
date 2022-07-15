efef8fdff8749ae1e65f2f788545f6320b28e617
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-07-15 12:44:26 -0400

    complete post functions

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,13 +1,43 @@
 import prisma from "../../lib/prisma";
 
-export default async function handler(req, res) {
-	if (req.method !== "GET") {
-		res.json(405).send({ message: "Only GET requests allowed" });
+export async function getAllPosts(req, res) {
+	if (req.method === "GET") {
+		const posts = await prisma.post.findMany({
+			where: { published: true },
+			include: { author: true },
+		});
+		res.json(posts);
 	}
+}
 
-	const posts = await prisma.post.findMany({
-		where: { published: true },
-		include: { author: true },
-	});
-	res.json(posts);
+export async function createPost(req, res) {
+	const { id } = req.query;
+
+	if (req.metHod === "POST") {
+		const result = await prisma.post.upsert({
+			create: {
+				data: {
+					...req.body,
+				},
+			},
+			update: {
+				data: {
+					...req.body,
+				},
+			},
+			where: { id: Number(id) },
+		});
+		res.json(result);
+	}
+}
+
+export async function deletePost(req, res) {
+	const { id } = req.query;
+
+	if (req.method === "DELETE") {
+		const result = await prisma.post.delete({
+			where: { id: Number(id) },
+		});
+		res.json({ message: "Post has been deleted" });
+	}
 }
