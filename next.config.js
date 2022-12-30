55a826b75c239ad58fea8fab57f3c9ee1f7cc330
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-12-30 17:59:58 -0500

    remove supabase

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	experimental: {
-		appDir: false, //NOTE: need to do more research on this
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: false, //NOTE: need to do more research on this
+  },
 };
 
 module.exports = nextConfig;
