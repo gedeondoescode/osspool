02487d44d109325f0bfc86521120de7f49257011
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2023-01-09 08:45:11 -0500

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import type { NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
-import SignIn from "./auth/signin";
 
 const Home: NextPage = () => {
 	const session = useSession();
