d93bc9d29505f702e610a2fd9337fd6a750b24d0
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2023-01-16 12:16:11 -0500

    complete GET request for profiles

--- pages/api/[username]/index.ts
+++ pages/api/[username]/index.ts
@@ -1,15 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
 import prisma from "../../../lib/prisma";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	const username = req.query.username;
+	const session = await unstable_getServerSession(req, res, authOptions);
 
 	if (req.method === "GET") {
-		const result = await prisma.profile.findUnique({
+		const result = await prisma.user.findUnique({
 			where: { username: String(username) },
+			select: {
+				name: true,
+				username: true,
+				bio: true,
+				posts: {
+					select: {
+						title: true,
+						_count: {
+							select: {
+								comments: true,
+								favorites: true,
+							},
+						},
+					},
+				},
+			},
 		});
 		res.json(result);
 	}
